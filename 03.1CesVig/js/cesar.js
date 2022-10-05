1cdc51f8e5a9f71bee5ef9ddbde2532879b6ccf1
IronFist482 <icernap2000@alumno.ipn.mx>
2022-10-04 22:44:13 -0500

    Tarea corregida

---
+++ 03.1CesVig/js/cesar.js
@@ -0,0 +1,108 @@
+
+
+
+var cesar =cesar || (function(){
+    var proceso= function(txt,desp,action){
+        
+        var replace=(function(){
+
+        //Primero necesito una matriz del abecedario
+        
+        var abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        var l = abc.length;
+        var desplazamientos=document.getElementById("desp").value;
+        desplazamientos=parseInt(desplazamientos);
+        
+        //Necesitamos una función que pueda obtener la posición que va a venir por parte de la clave privada o desplazamiento
+        return function(c){
+
+            
+
+
+            //Necesitamos saber la posición
+            var i=abc.indexOf(c.toLowerCase());
+            
+
+            //Necesitamos saber donde estamos dentro de la matriz abc y como la vamos a recorrer para el momento del cifrado
+            if(i!=-1){
+                //primero obtenemos la posición para el desplazamiento
+                
+                var pos;
+                if(l<(desplazamientos+i)){
+                    pos=(desplazamientos+i)-28;
+                }
+                else{
+                    pos=i;
+                }
+                
+
+                //necesito saber la operación a realizar
+                if(action){
+                    //cifrar hacia adelante
+                    
+                    pos+=desp-2;
+
+                    
+                    //definir cómo se va a mover
+                    pos-=(pos>=l)?1:0;
+                    console.log(pos);
+                }else{
+                    //descifra para atrás
+                    pos-=desp;
+                    //movimiento
+                    pos+=(pos<0)?1:0;
+                    console.log(pos);
+                }
+                return abc[pos];
+            }
+            return c;
+
+        };
+    })();
+    //tenemos que saber que el texto este acorde al abc
+
+    var re=(/([a-z])/ig);
+
+    //una función que se encarge de intercambio de las posiciones acorde a si coincide el texto a cifrar con la expresion regular
+
+    return String(txt).replace(re,function(match){
+        return replace(match);
+    });
+
+    };
+    return{
+        encode : function(txt,desp){
+            return proceso(txt, desp, true);
+        },
+        decode : function(txt,desp){
+            return proceso(txt, desp, false);
+        }
+        
+    };
+
+
+})();
+
+function cifrar(){
+    var desplazamientos=document.getElementById("desp").value;
+    desplazamientos=parseInt(desplazamientos);
+
+    if(desplazamientos<=26&&desplazamientos>0){
+        document.getElementById("resultado").innerHTML =cesar.encode(document.getElementById("cadena").value, desplazamientos);
+    }else{
+        alert("No pongas deplazamientos mayores a 26 ni menores a 1");
+        alert("miau miau :3");
+    }
+}
+function descifrar(){
+    var desplazamientos=document.getElementById("desp").value;
+    desplazamientos=parseInt(desplazamientos);
+
+    if(desplazamientos<=26&&desplazamientos>0){
+        document.getElementById("resultado").innerHTML =cesar.decode(document.getElementById("cadena").value, desplazamientos);
+    }else{
+        alert("No pongas deplazamientos mayores a 26 ni menores a 1");
+        alert("miau miau :3");
+    }
+    
+}
---
+++ Calculadora/src/calculadora/Operaciones.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author cerna
+ */
+public class Operaciones {
+    
+    public int suma(int x, int y){
+        return x+y;
+    }
+    
+    public int resta(int x, int y){
+        return x-y;
+    }
+    
+    public int multiplicacion(int x, int y){
+        return x*y;
+    }
+    
+    public int division(int x, int y){
+        return x/y;
+    }
+}
