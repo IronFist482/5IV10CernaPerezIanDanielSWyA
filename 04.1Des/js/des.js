7cd8b0405de869a13ace41a54c4db409bbf6492b
IronFist482 <icernap2000@alumno.ipn.mx>
2022-10-07 18:40:55 -0500

    Ya lo corregí u.u

--- 04.1Des/js/des.js
+++ 04.1Des/js/des.js
@@ -7,26 +7,31 @@ function descifrar(){
 
 }
 
-function guardarArchivo() {
+var guardar = document.querySelector(".save-as"),
+nombreArchivo = document.querySelector(".name-file");
 
-    var texto = document.getElementById("rs").value;
-    
-    var blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
-    FileSaver(blob, "cifrado.txt");
-}
-
-let archivo =document.getElementById('mess');
-
-let cifrado =document.getElementById('rs');
+var archivo =document.getElementById('mess'),
+cifrado =document.getElementById('rs');
 
 archivo.addEventListener('change',()=>{
     //Inicializo el lector del archivo
 
-    let lector =new FileReader();
-
+    var lector =new FileReader();
     lector.readAsText(archivo.files[0]);
-
     lector.onload =function(){
+
         document.getElementById('rs').innerHTML=lector.result;
+        const texto=lector.result;
+        guardar.addEventListener("click",() => {
+            console.log(texto);
+            var blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
+            var fileURL = URL.createObjectURL(blob),
+            link = document.createElement("a");
+            link.download = nombreArchivo.value;
+            link.href = fileURL;
+            link.click();
+        })        
     }
 })
+
+
